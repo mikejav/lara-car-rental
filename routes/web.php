f18f219f22d6bec58e27ca8c211448e7ed8d36d1
Michal Jaworski <mikejav@users.noreply.github.com>
2021-06-17 21:53:03 +0200

    update routing

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\VehicleController;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\RentalController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +19,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+})->name('home');
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::resource('vehicle', VehicleController::class);
+    Route::resource('customer', CustomerController::class);
+    Route::resource('rental', RentalController::class);
+    Route::get('rental/create/select-customer', [RentalController::class, 'selectCustomerStep'])->name('rental.create.selectCustomerStep');
+    Route::get('rental/create/select-vehicle-segment', [RentalController::class, 'selectVehicleSegmentStep'])->name('rental.create.selectVehicleSegmentStep');
+    Route::get('rental/create/select-vehicle', [RentalController::class, 'selectVehicleNarrowedToSegmentStep'])->name('rental.create.selectVehicleNarrowedToSegmentStep');
+    Route::get('rental/create/select-date-range', [RentalController::class, 'selectDateRangeStep'])->name('rental.create.selectDateRangeStep');
+    Route::get('rental/create/summary', [RentalController::class, 'summaryStep'])->name('rental.create.summary');
 });
+
+Route::get('login', [LoginController::class, 'index', 'as' => 'login'])->name('login');
+Route::post('login', [LoginController::class, 'attempt'])->name('login.attempt');
+Route::get('logout', [LoginController::class, 'logout'])->name('login.logout');
