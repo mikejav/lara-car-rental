e44cd52b6518c497f9455d48551fa300974788ea
Michal Jaworski <mikejav@users.noreply.github.com>
2021-06-21 23:41:15 +0200

    add local tw Bootstrap

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,8 +3,7 @@
 <head>
 <meta charset="UTF-8"/>
 <title>Car Rental</title>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-<link rel="stylesheet" href="style.css">
+<link href = {{ asset("/css/app.css") }} rel="stylesheet" />
 </head>
 <body class="d-flex flex-column h-100 bg-light">
 
